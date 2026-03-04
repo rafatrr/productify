a1fdbef62e51b1a2ade8dea9e8017e3d86c8f51e
Rafat khtab <rafat0khtab@gmail.com>
2026-03-04 18:22:12 +0300

    add frontend and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express();
+app.use(cors({origin: ENV.FRONTEND_URL}))
+app.use(clerkMiddleware());// auth obj will by attached to the req 
+app.use(express.json()); // parses json request bodies
+app.use(express.urlencoded({ extended: true})) //parses from data (link html forms )
+
+
+
+app.get('/', (req,res) => { 
+
+    res.json({ message: "welcome to productfy api - powerd by postgrsql, Drizzle orm, Clerk Auth",
+
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments"
+        }
+
+     })
+} )
+
+
+
+app.listen(ENV.PORT, () => console.log("Server is running on port", ENV.PORT))
