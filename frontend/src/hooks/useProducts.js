cd17f4abd2b19d2f7b2783b11db99b971fc543fa
Rafat khtab <rafat0khtab@gmail.com>
2026-04-13 05:32:04 +0300

    add creating and deleting commints. productPage , profilePage

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,9 +1,15 @@
 
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { createProduct, getAllProducts } from "../lib/api";
+import { createProduct, deleteProduct, getAllProducts, getMyProducts, getProductById } from "../lib/api";
+
+
 
 export const useProducts = () => {
-    const result = useQuery({ queryKey:['products'],queryFn:getAllProducts})
+    const result = 
+    useQuery({
+         queryKey:['products'],
+         queryFn:getAllProducts})
+
     return result;
 }
 
@@ -11,7 +17,28 @@ export const useCreateProduct = () => {
     const queryClient = useQueryClient();
     return useMutation({ mutationFn:createProduct,
          onSuccess:() => {
-        queryClient.invalidateQueries({ queryKey:['products'] })
+        queryClient.invalidateQueries({ 
+            queryKey:['products'] })
     }})
-    
+}
+export const useProduct = (id) => {
+     return useQuery({
+         queryKey:['product',id],
+         queryFn:() => getProductById(id),
+         enabled: !!id
+        })
+}
+export const useDeleteProduct = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {    
+      queryClient.invalidateQueries({ queryKey: ['myProducts'] });
+    }
+  })
+}
+
+
+export const useMyProducts = () => {
+   return useQuery({ queryKey: ["myProducts"], queryFn: getMyProducts });
 }
